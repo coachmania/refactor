21777d744cf58a971886ad4d37e0a444bd97971d
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-04 17:14:43 +0200

    add pinia in js files

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,5 +2,9 @@ import './assets/tailwind.css';
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import pinia from './store'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(pinia);
+app.mount('#app');
