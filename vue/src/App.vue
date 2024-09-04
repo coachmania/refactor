b54ccf9139fca9a09924f5f23f0d5b7a21bf411a
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-04 17:05:43 +0200

    add togle button

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <Home />
+    <Header/>
+    <Home/>
   </div>
 </template>
 
 <script>
 import Home from './components/Home.vue';
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
     Home,
+    Header,
   },
 };
 </script>
