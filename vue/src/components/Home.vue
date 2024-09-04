2dd6749405447b6682fe686fa64bcf02213e96f5
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-04 15:41:27 +0200

    add tailwind

--- vue/src/components/Home.vue
+++ vue/src/components/Home.vue
@@ -2,6 +2,8 @@
   <div class="home">
     <h1>Bienvenue sur la page d'accueil</h1>
     <p>Ceci est une simple page d'accueil créée avec Vue.js.</p>
+    <p class="text-lg text-center mt-4">Ceci est une simple page d'accueil créée avec Vue.js et Tailwind CSS.</p>
+    <h1 class= "text-blue-700">OKKKKKK</h1>
   </div>
 </template>
 
