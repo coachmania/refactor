0a19febd4496cb11a8bbf1a5935ea44a5fcb5bfd
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-04 16:08:08 +0200

    add tailwind config

--- vue/tailwind.config.js
+++ vue/tailwind.config.js
@@ -5,9 +5,61 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        card: '2.5rem',
+        icon: '1.25rem',
+      },
+      gridTemplateColumns: {
+        endDateLayout: 'calc(50% - 0.5rem) 1fr auto',
+      },
+      aspectRatio: {
+        a4: '1/1.414'
+      },
+      fontFamily: {
+        custom: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('daisyui'),
   ],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          'primary': '#f39200',
+          'primary-content': '#f4f4f4',
+          'base-100': '#f4f4f4',
+          'base-200': '#fafafa',
+          'base-300': '#c7c7c7',
+          'base-content': '#1d232a',
+          'info': '#0167d8',
+          'success': '#37b358',
+          'success-content': '#f4f4f4',
+          'warning': '#ef9400',
+          'error': '#eb4236',
+          'error-content': '#f4f4f4',
+        },
+      },
+      {
+        dark: {
+          ...require("daisyui/src/theming/themes")["dark"],
+          'primary': '#f39200',
+          'primary-content': '#f4f4f4',
+          'base-300': '#2e353d',
+          'base-content': '#f4f4f4',
+          'info': '#0167d8',
+          'success': '#37b358',
+          'success-content': '#f4f4f4',
+          'warning': '#ef9400',
+          'error': '#eb4236',
+          'error-content': '#f4f4f4',
+        },
+      },
+    ],
+    darkTheme: "dark",
+    base: false,
+  }
 }
