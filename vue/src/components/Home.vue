254c5c4b04dc5051b31aa35f83c4c5c3fa437dd9
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-04 15:31:15 +0200

    chnage home page in vue

---
+++ vue/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Bienvenue sur la page d'accueil</h1>
+    <p>Ceci est une simple page d'accueil créée avec Vue.js.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
