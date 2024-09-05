e79e6fb8ed45294fd4465cba0148d88540be219f
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-05 09:37:05 +0200

    add vue router

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,22 +1,11 @@
 <template>
-  <div id="app">
-    <Header/>
-    <Home/>
-  </div>
+	<div id="app">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-import Home from './components/Home.vue';
-import Header from './components/Header.vue';
-
 export default {
-  name: 'App',
-  components: {
-    Home,
-    Header,
-  },
+	name: 'App',
 };
 </script>
-
-<style>
-</style>
