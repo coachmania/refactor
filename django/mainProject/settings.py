8d178ea58ec086ae372727119622323f8cd61dd3
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-05 17:51:27 +0200

    add cors

--- django/mainProject/settings.py
+++ django/mainProject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'core',
     'accounts',
 ]
@@ -75,6 +76,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -83,6 +85,19 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'http://vue:5173',
+    'http://localhost:5173',
+    'http://django:8000',
+    'http://localhost:8000',
+    'https://localhost:8000',
+    'https://localhost:5173',
+    'https://localhost',
+    'http://localhost',
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'mainProject.urls'
 
