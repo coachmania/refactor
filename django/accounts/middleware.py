b254b0c6b2cb5d310a54dfd99d35f134acc0ee18
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-06 14:26:01 +0200

    delete middleware

--- django/accounts/middleware.py
+++ django/accounts/middleware.py
@@ -1,12 +1,12 @@
-class JWTAuthenticationMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
+# class JWTAuthenticationMiddleware:
+#     def __init__(self, get_response):
+#         self.get_response = get_response
 
-    def __call__(self, request):
-        access_token = request.COOKIES.get('access_token')
+#     def __call__(self, request):
+#         access_token = request.COOKIES.get('access_token')
 
-        if access_token:
-            request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
+#         if access_token:
+#             request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
 
-        response = self.get_response(request)
-        return response
+#         response = self.get_response(request)
+#         return response
