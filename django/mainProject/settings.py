a5f324b5a79e1e08ae2f5dbab619bc718288514d
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-06 17:15:13 +0200

    delete a middle ware

--- django/mainProject/settings.py
+++ django/mainProject/settings.py
@@ -79,7 +79,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'accounts.middleware.JWTAuthenticationMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
