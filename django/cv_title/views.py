9f504fc0367e590761476894615f4dd45f323856
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-06 17:57:44 +0200

    add title model view and app in settings

--- django/cv_title/views.py
+++ django/cv_title/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, serializers
+from .models import Title
 
-# Create your views here.
+class TitleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+class Update(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = TitleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- django/mainProject/settings.py
+++ django/mainProject/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'core',
     'accounts',
+    'cv_title',
 ]
 
 REST_FRAMEWORK = {
