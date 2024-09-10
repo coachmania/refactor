0d6b57e17565e9e33be7c6f1af9037ec6c0bf10c
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-10 17:41:29 +0200

    add button state

--- vue/src/components/global/QuillEditor.vue
+++ vue/src/components/global/QuillEditor.vue
@@ -1,13 +1,22 @@
 <template>
 	<div class="w-full">
 		<div id="toolbar" class="border border-base-300 rounded-t-btn flex p-1 gap-1">
-			<button class="ql-bold btn btn-sm btn-square btn-primary">
+			<button 
+				:class="{'btn-ghost': !isBoldActive, 'btn-primary': isBoldActive}" 
+				class="ql-bold btn btn-sm btn-square"
+			>
 				<svg class="w-icon h-icon fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17.061 11.22A4.46 4.46 0 0 0 18 8.5C18 6.019 15.981 4 13.5 4H6v15h8c2.481 0 4.5-2.019 4.5-4.5a4.48 4.48 0 0 0-1.439-3.28zM13.5 7c.827 0 1.5.673 1.5 1.5s-.673 1.5-1.5 1.5H9V7h4.5zm.5 9H9v-3h5c.827 0 1.5.673 1.5 1.5S14.827 16 14 16z"></path></svg>
 			</button>
-			<button class="ql-italic btn btn-sm btn-square btn-primary">
+			<button 
+				:class="{'btn-ghost': !isItalicActive, 'btn-primary': isItalicActive}" 
+				class="ql-italic btn btn-sm btn-square"
+			>
 				<svg class="w-icon h-icon fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 7V4H9v3h2.868L9.012 17H5v3h10v-3h-2.868l2.856-10z"></path></svg>
 			</button>
-			<button class="ql-underline btn btn-sm btn-square btn-primary">
+			<button 
+				:class="{'btn-ghost': !isUnderlineActive, 'btn-primary': isUnderlineActive}" 
+				class="ql-underline btn btn-sm btn-square"
+			>
 				<svg class="w-icon h-icon fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 18h14v2H5zM6 4v6c0 3.309 2.691 6 6 6s6-2.691 6-6V4h-2v6c0 2.206-1.794 4-4 4s-4-1.794-4-4V4H6z"></path></svg>
 			</button>
 		</div>
@@ -20,19 +29,36 @@ import { ref, onMounted } from 'vue';
 import Quill from 'quill';
 
 const editorContainer = ref(null);
+const isBoldActive = ref(false);
+const isItalicActive = ref(false);
+const isUnderlineActive = ref(false);
+
+const updateCurrentFormat = (quill) => {
+	const format = quill.getFormat();
+	isBoldActive.value = !!format.bold;
+	isItalicActive.value = !!format.italic;
+	isUnderlineActive.value = !!format.underline;
+};
 
 onMounted(() => {
-	new Quill(editorContainer.value, {
+	const quill = new Quill(editorContainer.value, {
 		modules: {
 			toolbar: '#toolbar'
 		}
+	});
+
+	quill.on('selection-change', () => {
+		updateCurrentFormat(quill);
+	});
+	quill.on('text-change', () => {
+		updateCurrentFormat(quill);
 	});
 });
 </script>
 
 <style>
 .ql-editor {
-	@apply min-w-full p-4 input h-full;
+	@apply w-full p-4 input h-full;
 }
 
 .ql-tooltip {
