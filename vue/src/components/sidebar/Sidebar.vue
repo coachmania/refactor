81ac737c6b7a9cac1d1991b0c9e100f366b26672
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-10 09:34:30 +0200

    add sidebar

---
+++ vue/src/components/sidebar/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+	<aside class="rounded-none bg-base-200 border-r border-base-300 flex flex-col items-center justify-center gap-4">
+		<a href="">ok</a>
+		<a href="">ok</a>
+		<a href="">ok</a>
+		<a href="">ok</a>
+		<a href="">ok</a>
+		<a href="">ok</a>
+	</aside>
+</template>
+
+<script>
+export default {
+	name: 'Sidebar',
+};
+</script>
