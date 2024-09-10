6ba55c3ab520615217a856a1b4a1e95c95f34e86
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-10 09:34:20 +0200

    add some layout

---
+++ vue/src/components/layout/EditorLayout.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="grid grid-cols-[5rem,4fr,5fr]">
+        <Sidebar/>
+        <div>
+            <slot name="editor"></slot>
+        </div>
+        <div>
+            <!-- <slot name="content"></slot> -->
+            <p>EDITOR</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidebar from '@/components/sidebar/Sidebar.vue';
+
+export default {
+    name: 'EditorLayout',
+    components: {
+        Sidebar,
+    }
+};
+</script>
---
+++ vue/src/components/layout/SectionLayout.vue
@@ -0,0 +1,11 @@
+<template>
+	<section class="border-r border-base-300 p-md flex flex-col gap-md overflow-auto h-full">
+		<slot name="cards"></slot>
+	</section>
+</template>
+
+<script>
+export default {
+    name: 'SectionLayout',
+};
+</script>
