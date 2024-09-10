e8d2016e27d4bd108f38fee93e556e33ccefd020
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-10 11:56:24 +0200

    add put and delete update in title

--- django/cv_title/urls.py
+++ django/cv_title/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import Update, Type
+from .views import Type
 
 urlpatterns = [
-    path('update/', Update.as_view(), name='update'),
     path('type/', Type.as_view(), name='type'),
 ]
--- django/cv_title/views.py
+++ django/cv_title/views.py
@@ -8,22 +8,10 @@ class TitleSerializer(serializers.ModelSerializer):
         model = Title
         fields = '__all__'
 
-class Update(APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = TitleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# TODO just for test 
-from rest_framework.permissions import AllowAny
 class Type(APIView):
-    permission_classes = [AllowAny]
-
     def get(self, request, *args, **kwargs):
         try:
-            # TODO temp
+            # TODO temp replace with cv=cv
             title = Title.objects.get_or_create(id=1)[0]
             type_labels = [choice[1] for choice in Title.TYPE_CHOICES]
             data = {
@@ -31,5 +19,14 @@ class Type(APIView):
                 'type_choices': type_labels
             }
             return Response(data, status=status.HTTP_200_OK)
+        except Title.DoesNotExist:
+            return Response({'error': 'Title not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, *args, **kwargs):
+        try:
+            title = Title.objects.get_or_create(id=1)[0]
+            title.type = request.data['type']
+            title.save()
+            return Response({'success': 'Title updated'}, status=status.HTTP_200_OK)
         except Title.DoesNotExist:
             return Response({'error': 'Title not found'}, status=status.HTTP_404_NOT_FOUND)
