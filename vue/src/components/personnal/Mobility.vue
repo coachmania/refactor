5e36f2a7a8fb84adabba3505b48c7976b60234cd
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-11 14:26:59 +0200

    add mobility vue

--- vue/src/components/personnal/Mobility.vue
+++ vue/src/components/personnal/Mobility.vue
@@ -8,8 +8,21 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
 import CardLayout from '../layout/CardLayout.vue';
 import CardTitle from '../global/CardTitle.vue';
 import AlertBox from '../global/AlertBox.vue';
 import TextInput from '../input/TextInput.vue';
+
+const fetchInitialData = async () => {
+	try {
+		const response = {data: {mobility: 'mobility', licenses: ['Aucun', 'BSR', 'Permis A', 'Permis B', 'Autre']}};
+	} catch (error) {
+		console.error('Erreur lors du chargement initial :', error);
+	}
+};
+
+onMounted(() => {
+	fetchInitialData();
+});
 </script>
