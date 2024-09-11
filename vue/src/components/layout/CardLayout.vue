56e795807e9eeee4cebf7fcd27694cebbd521c07
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-11 10:37:57 +0200

    add cardLayout

---
+++ vue/src/components/layout/CardLayout.vue
@@ -0,0 +1,10 @@
+<template>
+	<div class="card bg-base-200 border border-base-300">
+		<div class="grid p-card gap-md">
+			<slot name="content"></slot>
+		</div>
+	</div>
+</template>
+
+<script setup>
+</script>
