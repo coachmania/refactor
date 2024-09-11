2839022b36fcf0e0bb488effc2f61e2117e78c98
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-11 12:17:31 +0200

    add textInput in infos

--- vue/src/components/personnal/Infos.vue
+++ vue/src/components/personnal/Infos.vue
@@ -3,6 +3,50 @@
 		<CardTitle title="Informations personnelles"/>
 		<div class="grid grid-cols-2 gap-md">
 			<!-- TODO ADD text input -->
+			<TextInput
+				label="Prénom"
+				placeholder="Jean"
+				name="first_name"
+				:value="firstName"
+				@update:value="updateValue"
+			/>
+			<TextInput
+				label="Nom"
+				placeholder="Dupont"
+				name="name"
+				:value="name"
+				@update:value="updateValue"
+			/>
+			<TextInput
+				label="Téléphone"
+				placeholder="06 12 34 56 78"
+				name="phone"
+				:value="phone"
+				@update:value="updateValue"
+			/>
+			<TextInput
+				label="Email"
+				placeholder="jean@jobmania.fr"
+				name="email"
+				:value="email"
+				@update:value="updateValue"
+			/>
+			<!-- TODO replace by numberInput -->
+			<TextInput
+				label="Âge"
+				placeholder="20"
+				name="age"
+				:value="age"
+				@update:value="updateValue"
+			/>
+			<!-- TODO replace by dateInput -->
+			<TextInput
+				label="Date de naissance"
+				placeholder="01/01/2000"
+				name="birthdate"
+				:value="birthdate"
+				@update:value="updateValue"
+			/>
 		</div>
 	</CardLayout>
 </template>
@@ -10,4 +54,5 @@
 <script setup>
 import CardLayout from '../layout/CardLayout.vue';
 import CardTitle from '../global/CardTitle.vue';
+import TextInput from '../input/TextInput.vue';
 </script>
