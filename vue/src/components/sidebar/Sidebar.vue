a7f94ecb1c73498c195f685ea895eb9ed8a9362f
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-12 17:42:06 +0200

    add sidebar

--- vue/src/components/sidebar/Sidebar.vue
+++ vue/src/components/sidebar/Sidebar.vue
@@ -1,13 +1,22 @@
 <template>
 	<aside class="rounded-none bg-base-200 border-r border-base-300 flex flex-col items-center justify-center gap-4">
-		<a href="">ok</a>
-		<a href="">ok</a>
-		<a href="">ok</a>
-		<a href="">ok</a>
-		<a href="">ok</a>
-		<a href="">ok</a>
+		<SidebarButton
+			url="/personnal"
+			name="Informations personnelles"
+			:currentPage="true"
+		>
+			<svg class="h-icon w-icon fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M9.715 12c1.151 0 2-.849 2-2s-.849-2-2-2-2 .849-2 2 .848 2 2 2z"></path><path d="M20 4H4c-1.103 0-2 .841-2 1.875v12.25C2 19.159 2.897 20 4 20h16c1.103 0 2-.841 2-1.875V5.875C22 4.841 21.103 4 20 4zm0 14-16-.011V6l16 .011V18z"></path><path d="M14 9h4v2h-4zm1 4h3v2h-3zm-1.57 2.536c0-1.374-1.676-2.786-3.715-2.786S6 14.162 6 15.536V16h7.43v-.464z"></path></svg>
+		</SidebarButton>
+		<SidebarButton
+			url="/title"
+			name="Titre du CV"
+			:currentPage="false"
+		>
+	        <svg class="h-icon w-icon fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.903 8.586a.997.997 0 0 0-.196-.293l-6-6a.997.997 0 0 0-.293-.196c-.03-.014-.062-.022-.094-.033a.991.991 0 0 0-.259-.051C13.04 2.011 13.021 2 13 2H6c-1.103 0-2 .897-2 2v16c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2V9c0-.021-.011-.04-.013-.062a.952.952 0 0 0-.051-.259c-.01-.032-.019-.063-.033-.093zM16.586 8H14V5.414L16.586 8zM6 20V4h6v5a1 1 0 0 0 1 1h5l.002 10H6z"></path><path d="M8 12h8v2H8zm0 4h8v2H8zm0-8h2v2H8z"></path></svg>
+		</SidebarButton>
 	</aside>
 </template>
 
 <script setup>
+import SidebarButton from './SidebarButton.vue';
 </script>
---
+++ vue/src/components/sidebar/SidebarButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <a
+		class="btn btn-square grid place-items-center tooltip tooltip-right"
+		:class="currentPage ? 'btn-primary' : 'btn-ghost'"
+		:data-tip="name" 
+		:href="currentPage ? null : url">
+		<slot></slot>
+    </a>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+const props = defineProps({
+	url: String,
+	name: String,
+});
+
+const currentPage = ref(false);
+const route = useRoute();
+
+const checkCurrentPage = () => {
+	currentPage.value = route.path === props.url;
+};
+
+onMounted(checkCurrentPage);
+</script>
