b62a2ec0855f229f3e1db5d46470035211928dd1
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-12 18:01:05 +0200

    add temp template

--- vue/src/components/customize/Templates.vue
+++ vue/src/components/customize/Templates.vue
@@ -1,10 +1,18 @@
 <template>
 	<CardLayout>
 		<CardTitle>Modèles</CardTitle>
+		<div>
+			<TemplateCard 
+				v-for="n in 8"
+				:key="n" 
+				:template="n"
+			/>
+        </div>
 	</CardLayout>
 </template>
 
 <script setup>
 import CardLayout from '../layout/CardLayout.vue';
 import CardTitle from '../global/CardTitle.vue';
+import TemplateCard from './TemplateCard.vue';
 </script>
