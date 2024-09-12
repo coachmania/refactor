116c138802dc6748da812b977d92db8b05ac6152
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-12 15:24:51 +0200

    add checkbox input

--- vue/src/components/personnal/Mobility.vue
+++ vue/src/components/personnal/Mobility.vue
@@ -1,13 +1,21 @@
 <template>
 	<CardLayout>
 		<CardTitle>Mobilité</CardTitle>
-		<SelectInput
-			label="Permis"
-			name="license"
-			:value="data.license"
-			:items="data.license_choices"
-			@update:value="updateValue"
-		/>
+		<div class="col-span-full grid grid-cols-2 gap-md">
+			<SelectInput
+				label="Permis"
+				name="license"
+				:value="data.license"
+				:items="data.license_choices"
+				@update:value="updateValue"
+			/>
+			<CheckBoxInput
+				label="Véhicule"
+				name="has_vehicle"
+				:checked="data.has_vehicle"
+				@update:value="updateValue"
+			/>
+		</div>
 		<AlertBox classColor="alert-info">
 			La mobilité indique votre rayon de déplacement (ex : département, région, etc.).
 		</AlertBox>
@@ -21,6 +29,7 @@ import CardTitle from '../global/CardTitle.vue';
 import AlertBox from '../global/AlertBox.vue';
 import TextInput from '../input/TextInput.vue';
 import SelectInput from '../input/SelectInput.vue';
+import CheckBoxInput from '../input/CheckBoxInput.vue';
 
 const props = defineProps({
 	data: Object,
@@ -28,7 +37,7 @@ const props = defineProps({
 
 const updateValue = async ({name, value}) => {
 	try {
-		let sendData = {[name]: value,}
+		let sendData = {[name]: value}
 		await apiClient.put('/cv_personnal/fields/', sendData);
 	} catch (error) {
 		console.error(error);
