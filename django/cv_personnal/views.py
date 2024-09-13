6d4edc5a70a58b338b4c4b440cd0ee65227a045b
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-13 11:08:31 +0200

    delete unused get

--- django/cv_personnal/views.py
+++ django/cv_personnal/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Personnal
 
-from rest_framework.permissions import AllowAny
 class Infos(APIView):
     def get(self, request, *args, **kwargs):
         try:
@@ -37,33 +36,6 @@ class Mobility(APIView):
             return Response({'error': 'Personnal not found'}, status=status.HTTP_404_NOT_FOUND)
 
 class Fields(APIView):
-    permission_classes = [AllowAny]
-
-    def get(self, request, *args, **kwargs):
-        try:
-            personnal = Personnal.objects.get_or_create(id=1)[0]
-            license_choices = [choice[1] for choice in Personnal.LICENSE_CHOICES]
-            data = {
-                "license_choices": license_choices,
-                "name": personnal.name,
-                "first_name": personnal.first_name,
-                "phone": personnal.phone,
-                "email": personnal.email,
-                "age": personnal.age,
-                "birthdate": personnal.birthdate,
-                "additional": personnal.additional,
-                "postal_code": personnal.postal_code,
-                "city": personnal.city,
-                "country": personnal.country,
-                "license": personnal.license,
-                "other_license": personnal.other_license,
-                "has_vehicle": personnal.has_vehicle,
-                "range": personnal.range,
-            }
-            return Response(data, status=status.HTTP_200_OK)
-        except Personnal.DoesNotExist:
-            return Response({'error': 'Personnal not found'}, status=status.HTTP_404_NOT_FOUND)
-
     def put(self, request, *args, **kwargs):
         try:
             personnal = Personnal.objects.get_or_create(id=1)[0]
