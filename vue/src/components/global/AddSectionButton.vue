afccadaad28ed65e1b1875738574b77a62893132
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-13 09:47:41 +0200

    add square button

---
+++ vue/src/components/global/AddSectionButton.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="h-full"></div>
+	<div class="sticky bottom-md mt-md flex items-center justify-center">
+		<SquareButton>
+			<svg class="fill-current h-icon w-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path></svg>
+		</SquareButton>
+	</div>
+</template>
+
+<script setup>
+import SquareButton from '../button/SquareButton.vue';
+</script>
