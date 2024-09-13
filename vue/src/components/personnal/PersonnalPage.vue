a65e3c6c2a524f5f3e573e146046f8b739c616d4
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-13 11:02:54 +0200

    fix personnal Pae

--- vue/src/components/personnal/PersonnalPage.vue
+++ vue/src/components/personnal/PersonnalPage.vue
@@ -3,16 +3,14 @@
 	<EditorLayout>
 		<SectionLayout>
 			<Picture/>
-			<Infos v-model:data="pageData.data"/>
-			<Address v-model:data="pageData.data"/>
-			<Mobility v-model:data="pageData.data"/>
+			<Infos/>
+			<Address/>
+			<Mobility/>
 		</SectionLayout>
 	</EditorLayout>
 </template>
 
 <script setup>
-import { reactive, onMounted } from 'vue';
-import apiClient from '@/services/api';
 import Header from '@/components/header/Header.vue';
 import EditorLayout from '../layout/EditorLayout.vue';
 import SectionLayout from '../layout/SectionLayout.vue';
@@ -20,21 +18,4 @@ import Picture from './Picture.vue';
 import Infos from './Infos.vue';
 import Address from './Address.vue';
 import Mobility from './Mobility.vue';
-
-const pageData = reactive({
-	data: {},
-});
-
-const fetchData = async () => {
-	try {
-		const response = await apiClient.get('/cv_personnal/fields/');
-		Object.assign(pageData.data, response.data);
-	} catch (error) {
-		console.error('Error fetching title types:');
-	}
-};
-
-onMounted(() => {
-	fetchData();
-});
 </script>
