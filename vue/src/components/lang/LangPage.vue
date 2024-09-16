76929d35a6e20b9a4c77d5684cae90ef2946aeef
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-16 15:48:51 +0200

    add first lang front

--- vue/src/components/lang/LangPage.vue
+++ vue/src/components/lang/LangPage.vue
@@ -6,13 +6,16 @@
 			<AlertBox classColor="alert-info">
 				<p>La justification peut être une expérience à l'étranger ou une certification (ex : TOEIC).</p>
 			</AlertBox>
+            <div class="grid gap-md" v-if="items.length">
+                <LangItem v-for="lang in items" :key="lang.id" :lang="lang"/>
+            </div>
 			<AddSectionButton/>
 		</SectionLayout>
 	</EditorLayout>
 </template>
 
 <script setup>
-import { reactive, onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import apiClient from '@/services/api';
 import Header from '@/components/header/Header.vue';
 import EditorLayout from '../layout/EditorLayout.vue';
@@ -20,4 +23,20 @@ import SectionLayout from '../layout/SectionLayout.vue';
 import AddSectionButton from '../global/AddSectionButton.vue';
 import CardTitle from '../global/CardTitle.vue';
 import AlertBox from '../global/AlertBox.vue';
+import LangItem from './LangItem.vue';
+
+const items = ref([]);
+
+const fetchItems = async () => {
+	try {
+		const response = await apiClient.get('/cv_lang/items/');
+        items.value = response.data;
+	} catch (error) {
+		console.error('Error fetching languages:', error);
+	}
+};
+
+onMounted(() => {
+	fetchItems();
+})
 </script>
