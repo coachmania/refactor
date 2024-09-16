89a737819d542c38579060174b35ed7a255ce058
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-16 15:52:04 +0200

    fix some style

--- vue/src/components/lang/LangItem.vue
+++ vue/src/components/lang/LangItem.vue
@@ -4,7 +4,7 @@
 			<svg class="w-icon h-icon fill-current pointer-events-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="m7 17 5 5 5-5h-4V7h4l-5-5-5 5h4v10z"></path></svg>
 		</button>
 		<div class="grid grid-cols-[1fr,auto] items-center pl-md p-2 bg-base-200 border border-base-300 rounded-btn">
-			<h1>{{ lang.name }}</h1>
+			<h1 class="h-full flex items-center">{{ lang.name }}</h1>
 			<button class="btn btn-ghost btn-square">
 				<svg class="w-icon h-icon fill-error pointer-events-none" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>
 			</button>
