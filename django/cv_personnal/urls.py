6a592db1051a4857c37103480e383594b02951c3
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-17 15:37:18 +0200

    add picture api

--- django/cv_personnal/urls.py
+++ django/cv_personnal/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Infos, Address, Mobility, Fields
+from .views import Picture, Infos, Address, Mobility, Fields
 
 urlpatterns = [
+    path('picture/', Picture.as_view(), name='picture'),
     path('infos/', Infos.as_view(), name='infos'),
     path('address/', Address.as_view(), name='address'),
     path('mobility/', Mobility.as_view(), name='mobility'),
--- django/cv_personnal/views.py
+++ django/cv_personnal/views.py
@@ -3,6 +3,36 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Personnal
+import os, uuid
+
+class Picture(APIView):
+    def get(self, request, *args, **kwargs):
+        try:
+            personnal = Personnal.objects.get_or_create(id=1)[0]
+            return Response({
+                'picture': personnal.picture.url if personnal.picture else None,
+                'is_hidden': personnal.is_hidden
+            }, status=status.HTTP_200_OK)
+        except Personnal.DoesNotExist:
+            return Response({'error': 'Personnal not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, *args, **kwargs):
+        try:
+            personnal = Personnal.objects.get_or_create(id=1)[0]
+            if (personnal.picture):
+                personnal.picture.delete()
+            new_picture = request.FILES['picture']
+            extension = os.path.splitext(new_picture.name)[1]
+            new_filename = f"{uuid.uuid4()}{extension}"
+            new_picture.name = new_filename
+            personnal.picture = new_picture
+            try:
+                personnal.save()
+            except ValidationError as error:
+                return Response(error.message_dict, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'success': 'Picture updated'}, status=status.HTTP_200_OK)
+        except Personnal.DoesNotExist:
+            return Response({'error': 'Personnal not found'}, status=status.HTTP_404_NOT_FOUND)
 
 class Infos(APIView):
     def get(self, request, *args, **kwargs):
