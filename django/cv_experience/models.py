f22b822d089623fd9f7b3031eb9ab138bb4df521
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 09:30:55 +0200

    change order

--- django/cv_experience/models.py
+++ django/cv_experience/models.py
@@ -8,11 +8,11 @@ class Experience(models.Model):
 	city = models.CharField(max_length=100, blank=True)
 	contract = models.CharField(max_length=50, blank=True, default=CONTRACT_CHOICES[0])
 	details = models.TextField(blank=True)
-	order = models.PositiveIntegerField(null=True, blank=True, editable=False)
 	start_month = models.CharField(max_length=50, blank=True, default=MONTH_CHOICES[0])
 	start_year = models.PositiveIntegerField(null=True, blank=True)
 	end_month = models.CharField(max_length=50, blank=True, default=MONTH_CHOICES[0])
 	end_year = models.PositiveIntegerField(null=True, blank=True)
+	order = models.PositiveIntegerField(null=True, blank=True, editable=False)
 
 	def save(self, *args, **kwargs):
 		self.full_clean()
