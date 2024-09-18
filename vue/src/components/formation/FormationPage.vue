19963b85a3847417fabfd97733c460f13ebc9a5d
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 11:22:53 +0200

    add first formation pages

--- vue/src/components/formation/FormationPage.vue
+++ vue/src/components/formation/FormationPage.vue
@@ -2,16 +2,32 @@
 	<Header/>
 	<EditorLayout>
 		<SectionLayout>
-			<CardTitle>Formations</CardTitle>
+			<SubSectionLayout>
+				<template v-if="showDetail">
+					<ExperienceDetail :item_id="selectedId" @close="showDetail = false"/>
+				</template>
+				<template v-else>
+					<ExperienceItems @changeContent="handleChangeContent"/>
+				</template>
+			</SubSectionLayout>
 		</SectionLayout>
 	</EditorLayout>
 </template>
 
 <script setup>
-import { reactive, onMounted } from 'vue';
-import apiClient from '@/services/api';
+import { ref } from 'vue';
 import Header from '@/components/header/Header.vue';
 import EditorLayout from '../layout/EditorLayout.vue';
 import SectionLayout from '../layout/SectionLayout.vue';
-import CardTitle from '../global/CardTitle.vue';
+import SubSectionLayout from '../layout/SubSectionLayout.vue';
+import ExperienceDetail from './ExperienceDetail.vue';
+import ExperienceItems from './ExperienceItems.vue';
+
+const selectedId = ref();
+const showDetail = ref(false);
+
+const handleChangeContent = (id) => {
+	selectedId.value = id;
+    showDetail.value = true;
+};
 </script>
