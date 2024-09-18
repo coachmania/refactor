1198a0110f0a3367575d21538de7a38a0eca77bc
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 09:30:36 +0200

    add dates in front

--- vue/src/components/experience/ExperienceDetail.vue
+++ vue/src/components/experience/ExperienceDetail.vue
@@ -33,6 +33,41 @@
 			@update:value="updateValue"
 		/>
 	</div>
+	<CardTitle>Dates</CardTitle>
+	<div class="grid grid-cols-2 gap-md">
+		<SelectInput
+			label="Mois de début"
+			name="start_month"
+			:value="data.start_month"
+			:items="data.month_choices"
+			@update:value="updateValue"
+		/>
+		<NumberInput
+			:min="0"
+			:max="9999"
+			label="Année de début"
+			placeholder="2020"
+			name="start_year"
+			:value="data.start_year"
+			@update:value="updateValue"
+		/>
+		<SelectInput
+			label="Mois de fin"
+			name="end_month"
+			:value="data.end_month"
+			:items="data.month_choices"
+			@update:value="updateValue"
+		/>
+		<NumberInput
+			:min="0"
+			:max="9999"
+			label="Année de fin"
+			placeholder="2024"
+			name="end_year"
+			:value="data.end_year"
+			@update:value="updateValue"
+		/>
+	</div>
 	<AlertBox classColor="alert-info">
 		<p>La justification peut être une expérience à l'étranger ou une certification (ex : TOEIC).</p>
 	</AlertBox>
@@ -46,6 +81,7 @@ import TextInput from '../input/TextInput.vue';
 import AlertBox from '../global/AlertBox.vue';
 import ExitDetailButton from '../button/ExitDetailButton.vue';
 import SelectInput from '../input/SelectInput.vue';
+import NumberInput from '../input/NumberInput.vue';
 
 const data = reactive({});
 
@@ -61,6 +97,8 @@ const handleClick = () => {
 const updateValue = async ({name, value}) => {
 	try {
 		let sendData = {[name]: value,}
+		console.log(sendData);
+		
 		if (name === 'company') {
 			data.company = value;
 		}
