0c7e8fb8442af691b6362c3b2433d97241f62566
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 10:04:46 +0200

    add abstract class for date model

--- django/core/models.py
+++ django/core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from .constants import MONTH_CHOICES
 
-# Create your models here.
+class YearField(models.PositiveIntegerField):
+	def clean(self, value, model_instance):
+		if type(value) is str:
+			if value == "":
+				return None
+			if not value.isdigit():
+				raise ValidationError("L'année doit être un nombre")
+			value = int(value)
+		return value
+
+class ADateFields(models.Model):
+	start_month = models.CharField(max_length=50, blank=True, default=MONTH_CHOICES[0])
+	start_year = YearField(null=True)
+	end_month = models.CharField(max_length=50, blank=True, default=MONTH_CHOICES[0])
+	end_year = YearField(null=True)
+	is_current = models.BooleanField(default=False)
+
+	class Meta:
+		abstract = True
--- django/cv_experience/models.py
+++ django/cv_experience/models.py
@@ -1,17 +1,14 @@
 from django.db import models
-from core.constants import MONTH_CHOICES, CONTRACT_CHOICES
+from core.constants import CONTRACT_CHOICES
+from core.models import ADateFields
 
-class Experience(models.Model):
+class Experience(ADateFields):
 	# cv = models.ForeignKey(Cv, on_delete=models.CASCADE, related_name='experiences')
 	company = models.CharField(max_length=100, blank=True)
 	title = models.CharField(max_length=100, blank=True)
 	city = models.CharField(max_length=100, blank=True)
 	contract = models.CharField(max_length=50, blank=True, default=CONTRACT_CHOICES[0])
 	details = models.TextField(blank=True)
-	start_month = models.CharField(max_length=50, blank=True, default=MONTH_CHOICES[0])
-	start_year = models.PositiveIntegerField(null=True, blank=True)
-	end_month = models.CharField(max_length=50, blank=True, default=MONTH_CHOICES[0])
-	end_year = models.PositiveIntegerField(null=True, blank=True)
 	order = models.PositiveIntegerField(null=True, blank=True, editable=False)
 
 	def save(self, *args, **kwargs):
