04dd1c08b5777e93d90633d718e9ecfc571dab7a
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 14:24:31 +0200

    add all hobie page front

--- vue/src/components/hobbie/HobbiePage.vue
+++ vue/src/components/hobbie/HobbiePage.vue
@@ -2,16 +2,32 @@
 	<Header/>
 	<EditorLayout>
 		<SectionLayout>
-			<CardTitle>Centres d'intérêts</CardTitle>
+			<SubSectionLayout>
+				<template v-if="showDetail">
+					<HobbieDetail :item_id="selectedId" @close="showDetail = false"/>
+				</template>
+				<template v-else>
+					<HobbieItems @changeContent="handleChangeContent"/>
+				</template>
+			</SubSectionLayout>
 		</SectionLayout>
 	</EditorLayout>
 </template>
 
 <script setup>
-import { reactive, onMounted } from 'vue';
-import apiClient from '@/services/api';
+import { ref } from 'vue';
 import Header from '@/components/header/Header.vue';
 import EditorLayout from '../layout/EditorLayout.vue';
 import SectionLayout from '../layout/SectionLayout.vue';
-import CardTitle from '../global/CardTitle.vue';
+import SubSectionLayout from '../layout/SubSectionLayout.vue';
+import HobbieDetail from './HobbieDetail.vue';
+import HobbieItems from './HobbieItems.vue';
+
+const selectedId = ref();
+const showDetail = ref(false);
+
+const handleChangeContent = (id) => {
+	selectedId.value = id;
+    showDetail.value = true;
+};
 </script>
