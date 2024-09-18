ca1b371f222f7fa019137d32e6af781dcaed1506
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 10:58:13 +0200

    add global field for personnal

--- django/cv_personnal/urls.py
+++ django/cv_personnal/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import Picture, Infos, Address, Mobility, Fields
+from .views import Picture, Infos, Address, Mobility
+from core.views import FieldsSingle
+from .models import Personnal
 
 urlpatterns = [
     path('picture/', Picture.as_view(), name='picture'),
     path('infos/', Infos.as_view(), name='infos'),
     path('address/', Address.as_view(), name='address'),
     path('mobility/', Mobility.as_view(), name='mobility'),
-    path('fields/', Fields.as_view(), name='fields'),
+    path('fields/', FieldsSingle.as_view(model=Personnal), name='fields'),
 ]
--- django/cv_personnal/views.py
+++ django/cv_personnal/views.py
@@ -64,17 +64,3 @@ class Mobility(APIView):
             return Response(data, status=status.HTTP_200_OK)
         except Personnal.DoesNotExist:
             return Response({'error': 'Personnal not found'}, status=status.HTTP_404_NOT_FOUND)
-
-class Fields(APIView):
-    def put(self, request, *args, **kwargs):
-        try:
-            personnal = Personnal.objects.get_or_create(id=1)[0]
-            for key, value in request.data.items():
-                setattr(personnal, key, value)
-            try:
-                personnal.save()
-            except ValidationError as error:
-                return Response(error.message_dict, status=status.HTTP_400_BAD_REQUEST)
-            return Response({'success': 'Personnal updated'}, status=status.HTTP_200_OK)
-        except Personnal.DoesNotExist:
-            return Response({'error': 'Personnal not found'}, status=status.HTTP_404_NOT_FOUND)
