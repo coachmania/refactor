9fc78697f6aff5eed1207d2ee342d54b8e764e9b
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 16:01:57 +0200

    add first page layout

--- vue/src/components/layout/EditorLayout.vue
+++ vue/src/components/layout/EditorLayout.vue
@@ -2,13 +2,40 @@
     <main class="grid grid-cols-[5rem,4fr,5fr] overflow-hidden">
         <Sidebar/>
         <slot></slot>
-        <div>
-            <!-- <slot name="content"></slot> -->
-            <p>EDITOR</p>
+        <div ref="container" class="flex items-center justify-center bg-base-100 relative overflow-hidden select-none">
+            <div ref="page" class="bg-white w-[210mm] h-[297mm] origin-center pointer-events-none select-none aspect-a4 shadow-sm">
+                ok
+            </div>
         </div>
     </main>
 </template>
 
 <script setup>
+import { onMounted, ref, onBeforeUnmount } from 'vue';
 import Sidebar from '@/components/sidebar/Sidebar.vue';
+
+const container = ref(null);
+const page = ref(null);
+
+const adjustZoom = () => {
+    const containerWidth = container.value.clientWidth;
+    const containerHeight = container.value.clientHeight;
+    const divWidth = page.value.clientWidth;
+    const divHeight = page.value.clientHeight;
+
+    const scaleX = (containerWidth - 100) / divWidth;
+    const scaleY = (containerHeight - 100) / divHeight;
+    const scale = Math.min(scaleX, scaleY);
+
+    page.value.style.transform = `scale(${scale})`;
+};
+
+onMounted(() => {
+    adjustZoom();
+    window.addEventListener('resize', adjustZoom);
+});
+
+onBeforeUnmount(() => {
+    window.removeEventListener('resize', adjustZoom);
+});
 </script>
