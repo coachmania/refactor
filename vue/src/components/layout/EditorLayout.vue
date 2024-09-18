d7634ca60f06db6d9a3ac63811a74b20839dcda0
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 16:16:03 +0200

    add page zoom

--- vue/src/components/layout/EditorLayout.vue
+++ vue/src/components/layout/EditorLayout.vue
@@ -2,7 +2,11 @@
     <main class="grid grid-cols-[5rem,4fr,5fr] overflow-hidden">
         <Sidebar/>
         <slot></slot>
-        <div ref="container" class="flex items-center justify-center bg-base-100 relative overflow-hidden select-none">
+        <div 
+            ref="container" 
+            class="flex items-center justify-center bg-base-100 relative overflow-hidden select-none"
+            @wheel="handleMouseWheel"
+        >
             <div ref="page" class="bg-white w-[210mm] h-[297mm] origin-center pointer-events-none select-none aspect-a4 shadow-sm">
                 ok
             </div>
@@ -16,18 +20,34 @@ import Sidebar from '@/components/sidebar/Sidebar.vue';
 
 const container = ref(null);
 const page = ref(null);
+const scaleStep = 0.1;
+
+var currentScale;
+var translateX = 0;
+var translateY = 0;
+
+const updateTransform = () => {
+    page.value.style.transform = `translate(${translateX}px, ${translateY}px) scale(${currentScale})`;
+};
 
 const adjustZoom = () => {
-    const containerWidth = container.value.clientWidth;
-    const containerHeight = container.value.clientHeight;
-    const divWidth = page.value.clientWidth;
-    const divHeight = page.value.clientHeight;
+    currentScale = Math.min(
+        (container.value.clientWidth - 100) / page.value.clientWidth,
+        (container.value.clientHeight - 100) / page.value.clientHeight
+    );
+    updateTransform();
+};
 
-    const scaleX = (containerWidth - 100) / divWidth;
-    const scaleY = (containerHeight - 100) / divHeight;
-    const scale = Math.min(scaleX, scaleY);
+const handleMouseWheel = (event) => {
+    event.preventDefault();
+    const delta = event.deltaY < 0 ? 1 : -1;
 
-    page.value.style.transform = `scale(${scale})`;
+    if (event.ctrlKey) {
+        currentScale = Math.min(3.0, Math.max(0.5, currentScale + delta * scaleStep));
+    } else {
+        translateY += delta * 50;
+    }
+    updateTransform();
 };
 
 onMounted(() => {
