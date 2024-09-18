8d7864d066f5d06b8b61bc44e34f356db0ad9ed2
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-18 17:52:16 +0200

    add colors constant

---
+++ django/templates/colors.py
@@ -0,0 +1,29 @@
+DARK_COLORS = {
+	"darkBlue": "#1d3557",
+	"darkGreen": "#043a3c",
+	"darkAzur": "#006d77",
+	"darkViolet": "#6247aa",
+	"darkPink": "#b9375e",
+	"darkRed": "#6d071a",
+	"darkBrown": "#7f5539",
+	"darkGray": "#353535",
+}
+
+LIGHT_COLORS = {
+	"lightBlue": "#d1e2ef",
+	"lightGreen": "#bbd4d5",
+	"lightAzur": "#9ad1d4",
+	"lightViolet": "#ebd9fc",
+	"lightPink": "#ffcbf2",
+	"lightRed": "#f1cdcd",
+	"lightBrown": "#e6ccb2",
+	"lightGrey": "#e7ecef",
+}
+
+NEUTRAL_DARK_COLORS = {
+	"darkA": "#212529",
+}
+
+NEUTRAL_LIGHT_COLORS = {
+	"lightA": "#f8f9fa"
+}
