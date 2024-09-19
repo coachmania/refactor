deb3da741fdea59e0888e6d9609e2f1bfda76f9b
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-19 10:20:30 +0200

    fix style

--- vue/src/components/customize/ColorSelector.vue
+++ vue/src/components/customize/ColorSelector.vue
@@ -3,7 +3,7 @@
 	<div class="grid grid-cols-8 gap-2">
 		<button v-for="color in colors">
 			<div
-				class="btn btn-ghost btn-block h-12 rounded-btn"
+				class="btn btn-ghost btn-block h-12 border border-base-content/15"
 				:style="{ backgroundColor: color }"
 			></div>
 			<p class="text-center">•</p>
