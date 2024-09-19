3cc16c41564a89b0579f7357b7d872bffaee6c2d
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-19 11:41:09 +0200

    add templateAlice

--- vue/src/components/template/TemplateAlice.vue
+++ vue/src/components/template/TemplateAlice.vue
@@ -1,8 +1,16 @@
 <template>
+	<div class="box-border overflow-hidden w-full h-full grid grid-cols-[80mm,1fr] bg-tpPrimary text-tpBody">
+		<aside class="p-10 rayClass">
+			<div class="bg-tpLight h-full">
 
-	
+			</div>
+		</aside>
+	</div>
 </template>
 
-<script setup>
-
-</script>
+<style scoped>
+.rayClass {
+	background: linear-gradient(-45deg, transparent 22.5%, #ffffff88 22.5%, #ffffff88 27.5%, transparent 27.5%, transparent 72.5%, #ffffff88 72.5%, #ffffff88 77.5%, transparent 77.5%);
+	background-size: 20px 20px;
+}
+</style>
