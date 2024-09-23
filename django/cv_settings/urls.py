e867363b3f47e998f47d4284b4c11cca3f21570a
Elias ZANOTTI <zanotti.elias@gmail.com>
2024-09-23 16:34:06 +0200

    add scheme in cv_settings

--- django/cv_settings/urls.py
+++ django/cv_settings/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Fields
+from .views import Fields, Scheme
 
 urlpatterns = [
     path('fields/', Fields.as_view(), name='fields'),
+	path('scheme/<int:id>/', Scheme.as_view(), name='scheme'),
 ]
--- django/cv_settings/views.py
+++ django/cv_settings/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from rest_framework import serializers
 from .models import Settings
 from cv.getObjects import getSingleObject
+from .schemes import TEMPLATES
 
 class SettingsSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -14,3 +15,11 @@ class Fields(APIView):
 	def get(self, request):
 		serializer = SettingsSerializer(getSingleObject(request, Settings))
 		return Response(serializer.data, status=status.HTTP_200_OK)
+
+class Scheme(APIView):
+	def get(self, request, id, *args, **kwargs):
+		try:
+			scheme = TEMPLATES[id]
+			return Response(scheme, status=status.HTTP_200_OK)
+		except:
+			return Response({'error': 'Color not found'}, status=status.HTTP_404_NOT_FOUND)
